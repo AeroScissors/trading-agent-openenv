994e4b295a26f3331c0dc62c04c980d970d92fab
Aero Scissors <ys087766@gmail.com>
2026-03-28 01:21:02 +0530

    Fixed startup data generation using generate_all

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 from api.routes import router
+import os
+
+# ✅ correct import
+from generate_data import generate_all
 
 app = FastAPI()
 
 app.include_router(router)
+
+@app.on_event("startup")
+def startup_event():
+    print("🚀 Starting API...")
+
+    data_path = "data/prices/aapl.csv"
+
+    if not os.path.exists(data_path):
+        print("📦 Generating data...")
+        generate_all()   # ✅ correct function
+        print("✅ Data ready!")
+    else:
+        print("✅ Data already exists")
 
 @app.get("/")
 def root():
